aa2ceecf141eed976242d4c3cc9a44985d57c61c
Micky Cheng <mickycm@users.noreply.github.com>
2016-06-27 15:16:26 +0800

    Update quick_sort.php

--- quick_sort.php
+++ quick_sort.php
@@ -3,19 +3,23 @@
 
 //要排序的数组
 function QuickSort(&$arr = []){
-   $len = strlen($arr);
+   $len = count($arr);
    if($len <=1) return $arr;
-   $pivot  = $arr[$len-1];   //选最后一个作基准元素
+   $pivot  = $arr[0];   //选第一个作基准元素
    $leftArr = [];
    $rightArr = [];  
-   for($i = 0; $i <= $len; $i++){
-      if($arr[$i] < $pivot){
+   for($i = 1; $i < $len; $i++){   //第二个开始比较
+      if($arr[$i] <= $pivot){
          $leftArr[]  = $arr[$i];
       }else{
           $rightArr[] =  $arr[$i];
       }
    }//end for
-   QuickSort($leftArr);
-   QuickSort($rightArr);
-  
+  $left =  QuickSort($leftArr);    //左边排序
+   $right =    QuickSort($rightArr);  //右边排序
+  return array_merge($left,[$pivot],$right);
 }
+
+$arr = [5,2,3,1,23,9,5,3,11,34,5,23,12,1,22,12,13,14,8,7];
+$sortArr = QuickSort($arr);
+print_r($sortArr);
